8e8fc4eb08b49585fb6bf5e4975420fcf558af5e
Yichan Kim <yichankim@gmail.com>
2025-10-26 19:05:44 +0000

    feat: add launch_program() and child()
    Implements process creation using fork() and execution with execvp(). Parent handles the exit command

--- s3.c
+++ s3.c
@@ -42,26 +42,63 @@ void parse_command(char line[], char *args[], int *argsc)
     args[*argsc] = NULL; ///args must be null terminated
 }
 
-///Launch related functions
+/**
+ * child
+ * 
+ * Use execvp to load the binary of the command specified in args[ARG_PROGNAME].
+ * 
+ * Reference: Lecture 3
+ * 
+ * args = array of strings. args contains the command and its arguments
+ * argsc = number of strings in args
+ * 
+ * We do not need to worry about empty args because it is taken care of at launch_program
+ */
 void child(char *args[], int argsc)
 {
-    ///Implement this function:
+    //transforms the current process into a new program
+    execvp(args[0], args);
 
-    ///Use execvp to load the binary 
-    ///of the command specified in args[ARG_PROGNAME].
-    ///For reference, see the code in lecture 3.
+    //if execvp is successful, we will not get here so we can do error handling here
+    fprintf(stderr, "Incorrect args\n");
+    exit(1);
+
+
 }
 
+
+/**
+ * launch_program
+ * 
+ * fork() a child process.
+ * In the child part of the code, call child(args, argv)
+ *
+ * Reference: Lecture 2 
+ *  
+ * args = array of strings. args contains the command and its arguments
+ * argsc = number of strings in args
+ * 
+ * Edge case to handle: 
+ *  1) Empty args **MUST BE CONSIDERED**
+ *  2) "exit" command: shell (not the child) should exit
+ *
+ */
 void launch_program(char *args[], int argsc)
 {
-    ///Implement this function:
 
-    ///fork() a child process.
-    ///In the child part of the code,
-    ///call child(args, argv)
-    ///For reference, see the code in lecture 2.
+    if (strcmp(args[0], "exit")==0){
+        exit(0); //success status code
+    }
+    int rc = fork();
+    if (rc < 0) { //fork failed, exit
+        fprintf(stderr, "fork failed\n");
+        exit(1); // error status code
+    } else if (rc==0) { //child process, run child
+        child(args, argsc);
+    } else { //parent node
+        int wc = wait(NULL); //wait for child process to finish    
+    }
+    return;
 
-    ///Handle the 'exit' command;
-    ///so that the shell, not the child process,
-    ///exits.
+
 }
