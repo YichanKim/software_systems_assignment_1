7a54d6a05c3962b4783ec301b9724e0930887c0f
Yichan Kim <yichankim@gmail.com>
2025-12-03 15:36:54 +0000

    s3main.c bug fix and nested subshell implementation \ - Synchronised the order of checking/running for commands the while(1) so that it matches that of the if (argc > 1) loop \ - Added

--- s3main.c
+++ s3main.c
@@ -1,54 +1,5 @@
 #include "s3.h"
 
-//This is the main function that runs the shell.
-//It reads the command line input, parses the command, and launches the program. 
-//It also reaps the child process.
-//It's in an infinite loop so it can keep reading and launching programs.
-/*
-int main(int argc, char *argv[]){
-
-    ///Stores the command line input
-    char line[MAX_LINE];
-
-    ///Stores pointers to command arguments.
-    ///The first element of the array is the command name.
-    char *args[MAX_ARGS];
-
-    ///Stores the number of arguments
-    int argsc;
-
-    while (1) { //Edit the main loop to handle redirection and pipes
-        read_command_line(line);
-
-        if (command_with_pipes(line)) {
-            char *pipeline_cmds[MAX_ARGS];
-            int pipeline_count = 0;
-
-            if (tokenize_pipeline(line, pipeline_cmds, &pipeline_count)) { //Split the line into separate command strings so that each command is parsed
-                launch_pipeline(pipeline_cmds, pipeline_count); //Handles launching each command
-            } else {
-                fprintf(stderr, "Pipeline parse error\n"); //Error handling
-            }
-
-            reap();
-
-        } else if (command_with_redirection(line)) { //Inspect the raw line for '>', '>>' or '<', handle it with separate functions
-            parse_command(line, args, &argsc);
-            launch_program_with_redirection(args, argsc);
-            reap();
-        } else { //Else, just launch as normal
-            parse_command(line, args, &argsc);
-            launch_program(args, argsc);
-            reap();
-        }
-    }
-
-    return 0;
-    
-}
-*/
-
-
 int main(int argc, char *argv[]){
     ///Stores the command line input
     char line[MAX_LINE];
@@ -98,6 +49,18 @@ int main(int argc, char *argv[]){
             parse_command(line, args, &argsc);
             launch_program_with_redirection(args, argsc);
             reap();
+            
+        }
+        //Checks for subshell and runs subshell. Allows for implementation of nested subshell
+        //Allows for commands like ((((echo "HI"))))
+        else if (command_with_subshell(line)) { 
+            char subshell_cmd[MAX_LINE];
+            //extracts subshell and laucnhes subshell
+            if (extract_subshell_commands(line, subshell_cmd)){
+                launch_subshell(subshell_cmd);
+            }
+            reap();
+
         }
         else {
             parse_command(line, args, &argsc);
@@ -113,22 +76,7 @@ int main(int argc, char *argv[]){
 
         read_command_line(line, lwd); ///Notice the additional parameter (required for prompt construction)
 
-        if(is_cd(line)){///Implement this function
-            parse_command(line, args, &argsc);
-            run_cd(args, argsc, lwd);
-        }
-        else if(command_with_subshell(line)){
-            char subshell_cmd[MAX_LINE];
-
-            if (extract_subshell_commands(line, subshell_cmd)){
-                launch_subshell(subshell_cmd);
-            } else {
-                fprintf(stderr, "Subshell command syntax error\n");
-            }
-
-            reap();
-        }
-        else if(command_with_batch(line)){
+        if(command_with_batch(line)){
             char *batch_cmds[MAX_ARGS];
             int batch_count = 0;
 
@@ -137,6 +85,10 @@ int main(int argc, char *argv[]){
             } else {
                 fprintf(stderr, "Batch parse error\n");
             }
+        }
+        else if(is_cd(line)){///Implement this function
+            parse_command(line, args, &argsc);
+            run_cd(args, argsc, lwd);
         }
         else if(command_with_pipes(line)){
             char *pipeline_cmds[MAX_ARGS];
@@ -155,7 +107,17 @@ int main(int argc, char *argv[]){
            parse_command(line, args, &argsc);
            launch_program_with_redirection(args, argsc);
            reap();
-       }
+        } 
+       else if(command_with_subshell(line)){
+            char subshell_cmd[MAX_LINE];
+
+            if (extract_subshell_commands(line, subshell_cmd)){
+                launch_subshell(subshell_cmd);
+            } else {
+                fprintf(stderr, "Subshell command syntax error\n");
+            }
+            reap();
+        }
        else ///Basic command
        {
            parse_command(line, args, &argsc);
